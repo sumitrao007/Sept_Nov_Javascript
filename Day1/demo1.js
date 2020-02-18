8d1028a6e8e6491d57c891c06711a81c4c54dac4
sumitrao007 <raokhande.sumit@gmail.com>
2020-02-18 13:46:06 +0530

    Day1

---
+++ Day1/demo1.js
@@ -0,0 +1,55 @@
+// var a1;
+// a1="Welcome to JavaScript Module";
+// console.log(a1);
+// //Data type
+// // String,number(float),boolean,undefined
+// console.log("A1 has data type id "+typeof a1);
+// console.log("A1 has data type id using Method "+typeof(a1));
+// a1=2;
+// console.log("Value of a1 is "+a1);
+// console.log("Datatype of a1 is "+typeof a1);
+// a1=true;
+// console.log("Value of a1 is "+a1);
+// console.log("Datatype of a1 is "+typeof a1);
+// a1="Hi Hopes So u are not Bored!!!!!!!";
+// console.log("Datatype is "+typeof a1);
+// console.log("VALUE  is "+a1);
+// //OPERATOR arithematic,logical,relational,bitwise,ternary,assignment
+// //+ - * / %
+// //&& || ^ !
+// //> < >= <= == === !=
+// //& | ~
+// //expression(condition)?value1:value2
+// //= += -= *= 
+
+// //if(){}, if(){}else{},if(){}else if(){}else{}
+// var a2=5;
+// var a3=10;
+// if(a2>a3){
+// console.log("A2 is greater "+a2);
+// }else{
+//     console.log("A3 is greater "+a3);
+// }
+
+// //For loop same as Java
+// for(let i=1;i<=5;i++){
+//     console.log("Value of i is "+i);
+// }
+// //console.log("value of i is "+i);
+// const i=3.14;
+// console.log("Const Value is "+i);
+// //i=4;
+// console.log("Value of is After changed i "+i);
+var i=44;
+for (let i = 0; i < 4; i++) {
+    console.log("In For Loop i is "+i); 
+    const p=i;
+    console.log(p);   
+}
+
+console.log("Value of i is After For Loop "+i);
+//For you Reference
+// const i=3.14;
+// console.log("Value of I as const "+i);
+
+
---
+++ Day1/demo2.js
@@ -0,0 +1,6 @@
+console.log("Hello World!!!!!!");
+
+document.write("Hello World....");
+var a=4;
+document.write("Value of A is "+a);
+
